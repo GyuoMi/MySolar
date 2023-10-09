d7c39b7622d9b4ed3670e1ebd672172ed662798c
Sahil Dinanath <sahildinanath@debian.local>
2023-10-09 14:46:49 +0200

    implemented LoggedInUser class

---
+++ my_solar_app/lib/models/logged_in_user.dart
@@ -0,0 +1,49 @@
+class LoggedInUser {
+  static int userId = 0, systemId = 0;
+  static String userEmail = "", userPassword = "", userAddress = "";
+
+  static setUser(
+      int id, int sysId, String email, String password, String address) {
+    userId = id;
+    systemId = sysId;
+    userEmail = email;
+    userPassword = password;
+    userAddress = address;
+  }
+
+  static getuserId() {
+    return userId;
+  }
+
+  static getSystemId() {
+    return systemId;
+  }
+
+  static getEmail() {
+    return userEmail;
+  }
+
+  static getPassword() {
+    return userPassword;
+  }
+
+  static getAddress() {
+    return userAddress;
+  }
+
+  static setSystemId(int id) {
+    systemId = id;
+  }
+
+  static setEmail(String email) {
+    userEmail = email;
+  }
+
+  static setPassword(String password) {
+    userPassword = password;
+  }
+
+  static setAddress(String address) {
+    userAddress = address;
+  }
+}
