bc02e4a7fb493b1f28f25edc3547167ecd15ecf0
Sahil Dinanath <sahildinanath@debian.local>
2023-10-21 20:01:37 +0200

    implemented interface to interact with database functions

--- my_solar_app/lib/cloud_functions/database/interfaces/database_functions_interface.dart
+++ my_solar_app/lib/cloud_functions/database/interfaces/database_functions_interface.dart
@@ -1,9 +1,3 @@
 abstract class IDatabaseFunctions {
-  Future createRecord(String table, Map insert);
-  Future readRecordsWhere(String table, Map matchesFields);
-  Future readRecords(String table);
-  Future updateField(String table, Map updatesColumn, Map matchesFields);
-  Future deleteRecord(String table, Map matchesFields);
-  Future databaseFunction(
-      String functionName, Map<String, dynamic> matchesFields);
+  Future calculateAllTimeTotals(int id);
 }
