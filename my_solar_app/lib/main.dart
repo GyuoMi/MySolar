edf66423d0716c0fdbac3d759a0bf7e0380227bc
GyuoMi <darshnsingh10@gmail.com>
2023-11-07 09:11:38 +0200

    trying to grant notification permissions
    notification scheduling code is in main.dart but the await is causing the whole app to hang on launch so wip

--- my_solar_app/lib/main.dart
+++ my_solar_app/lib/main.dart
@@ -5,9 +5,29 @@ import 'package:my_solar_app/screens/register_user_page.dart';
 import 'package:my_solar_app/screens/Homepage.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:my_solar_app/screens/devices.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:timezone/timezone.dart' as tz;
+import 'package:timezone/data/latest.dart' as tz;
+import 'package:permission_handler/permission_handler.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  tz.initializeTimeZones();
+
+  final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+  // Check notification permission status
+  final permissionStatus = await Permission.notification.status;
+  if (permissionStatus.isDenied) {
+    // You can request notification permissions here
+    final status = await Permission.notification.request();
+
+    if (status.isGranted) {
+      print('Notification permission granted');
+    } else {
+      print('Notification permission denied');
+    }
+  }
+  await scheduleNotifications(flutterLocalNotificationsPlugin);
 
   await Supabase.initialize(
     url: 'https://fsirbhoucrjtnkvchwuf.supabase.co',
@@ -15,6 +35,37 @@ Future<void> main() async {
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzaXJiaG91Y3JqdG5rdmNod3VmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTIzNzYxNTAsImV4cCI6MjAwNzk1MjE1MH0.Bb3OZyxku8_7c_aIQe5GlMsup0SODK-5pPa92tzkNFM',
   );
   runApp(MyApp());
+}
+
+Future<void> scheduleNotifications(FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
+  // Define your notification details outside the loop
+  const AndroidNotificationDetails androidPlatformChannelSpecifics = AndroidNotificationDetails(
+    '0', // Replace with your own channel ID
+    'Scheduled Notifications',
+    channelDescription: 'Scheduled Notifications Channel',
+    importance: Importance.defaultImportance,
+    priority: Priority.defaultPriority,
+  );
+  const NotificationDetails platformChannelSpecifics = NotificationDetails(android: androidPlatformChannelSpecifics);
+
+  // Calculate the time for the first notification at midnight
+  final now = tz.TZDateTime.now(tz.local);
+  final midnight = tz.TZDateTime(tz.local, now.year, now.month, now.day, 0, 0);
+
+  // Schedule notifications every 4 hours starting from midnight
+  for (int i = 0; i < 6; i++) {
+    final scheduledTime = midnight.add(Duration(hours: i * 4));
+    await flutterLocalNotificationsPlugin.zonedSchedule(
+      i, // Unique ID for the notification
+      'MySolar',
+      'Click to enter your device readings!',
+      scheduledTime,
+      platformChannelSpecifics, // Reuse the notification details here
+      androidAllowWhileIdle: true,
+      payload: 'tracking_page.dart',
+      uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime,
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
