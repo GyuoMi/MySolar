9dc6716526f5c5624401e76f7d3c91b031f0c2dc
GyuoMi <darshnsingh10@gmail.com>
2023-11-07 21:49:18 +0200

    working notification

--- my_solar_app/lib/main.dart
+++ my_solar_app/lib/main.dart
@@ -8,64 +8,22 @@ import 'package:my_solar_app/screens/devices.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/timezone.dart' as tz;
 import 'package:timezone/data/latest.dart' as tz;
-import 'package:permission_handler/permission_handler.dart';
+import 'package:my_solar_app/utilities/notifications.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  tz.initializeTimeZones();
-
-  final flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
-  // Check notification permission status
-  final permissionStatus = await Permission.notification.status;
-  if (permissionStatus.isDenied) {
-    // You can request notification permissions here
-    final status = await Permission.notification.request();
-
-    if (status.isGranted) {
-      print('Notification permission granted');
-    } else {
-      print('Notification permission denied');
-    }
-  }
-  await scheduleNotifications(flutterLocalNotificationsPlugin);
-
+  final notificationService = NotificationService(); // Create an instance
+  await notificationService.initNotification(); // Initialize notifications
+  await notificationService.showNotification(
+    title: 'MySolar',
+    body: 'Enter new device readings',
+  );
   await Supabase.initialize(
     url: 'https://fsirbhoucrjtnkvchwuf.supabase.co',
     anonKey:
     'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZzaXJiaG91Y3JqdG5rdmNod3VmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTIzNzYxNTAsImV4cCI6MjAwNzk1MjE1MH0.Bb3OZyxku8_7c_aIQe5GlMsup0SODK-5pPa92tzkNFM',
   );
   runApp(MyApp());
-}
-
-Future<void> scheduleNotifications(FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
-  // Define your notification details outside the loop
-  const AndroidNotificationDetails androidPlatformChannelSpecifics = AndroidNotificationDetails(
-    '0', // Replace with your own channel ID
-    'Scheduled Notifications',
-    channelDescription: 'Scheduled Notifications Channel',
-    importance: Importance.defaultImportance,
-    priority: Priority.defaultPriority,
-  );
-  const NotificationDetails platformChannelSpecifics = NotificationDetails(android: androidPlatformChannelSpecifics);
-
-  // Calculate the time for the first notification at midnight
-  final now = tz.TZDateTime.now(tz.local);
-  final midnight = tz.TZDateTime(tz.local, now.year, now.month, now.day, 0, 0);
-
-  // Schedule notifications every 4 hours starting from midnight
-  for (int i = 0; i < 6; i++) {
-    final scheduledTime = midnight.add(Duration(hours: i * 4));
-    await flutterLocalNotificationsPlugin.zonedSchedule(
-      i, // Unique ID for the notification
-      'MySolar',
-      'Click to enter your device readings!',
-      scheduledTime,
-      platformChannelSpecifics, // Reuse the notification details here
-      androidAllowWhileIdle: true,
-      payload: 'tracking_page.dart',
-      uiLocalNotificationDateInterpretation: UILocalNotificationDateInterpretation.absoluteTime,
-    );
-  }
 }
 
 class MyApp extends StatelessWidget {
